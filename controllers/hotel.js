7fb7044948c4572d2234999c9982848d7e996acf
jiboladestiny <jibola1444@gmail.com>
2022-09-26 14:29:36 +0100

    Changed country by city query

--- controllers/hotel.js
+++ controllers/hotel.js
@@ -45,6 +45,9 @@ export const getHotels = async (req, res, next) => {
      delete req.query[key];
    }
  });
+
+ console.log(req.query)
+
   const { min, max, ...others } = req.query;
   try {
     const hotels = await Hotel.find({
@@ -79,10 +82,10 @@ export const countByType = async (req, res, next) => {
 
     res.status(200).json([
       { type: "hotel", count: hotelCount },
-      { type: "apartments", count: apartmentCount },
-      { type: "resorts", count: resortCount },
-      { type: "villas", count: villaCount },
-      { type: "cabins", count: cabinCount },
+      { type: "apartment", count: apartmentCount },
+      { type: "resort", count: resortCount },
+      { type: "villa", count: villaCount },
+      { type: "cabin", count: cabinCount },
     ]);
   } catch (err) {
     next(err);
